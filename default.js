1e3aa5ca873cb949da9390f23423e088f13724aa
Douglas Crisona <douglascrisona@gmail.com>
2016-05-30 13:47:12 -0700

    Added Data Object to Default.js .#1

---
+++ default.js
@@ -0,0 +1,13 @@
+// Tennis Products
+var products = [
+  { name: 'Babolat Aeropro' , price: 120.95},
+  { name: 'Wilson Pro Statt 97', price: 199.00},
+  { name: 'Penn Tennis Ball Champ 3pk', price: 2.04},
+  { name: 'NikeCourt Premier RF Emoji Head', price: 35.00},
+  { name: 'Nike Vapor Court', price: 49.95},
+  { name: 'Head Tour Team Tennis Bag', price: 39.95},
+]
+
+
+// Matches the search term with an object property string name
+// If equal, append restuls to the page
